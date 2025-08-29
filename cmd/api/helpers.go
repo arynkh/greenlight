e462f00c2761f4c221852427db835d04810bc610
arynkh <akhead1@crimson.ua.edu>
2025-08-28 23:03:32 -0500

    add custom json decoding for runtime

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -7,6 +7,7 @@ import (
 	"io"
 	"net/http"
 	"strconv"
+	"strings"
 
 	"github.com/julienschmidt/httprouter"
 )
@@ -51,12 +52,18 @@ func (app *application) writeJSON(w http.ResponseWriter, status int, data envelo
 }
 
 func (app *application) readJSON(w http.ResponseWriter, r *http.Request, dst any) error {
+	r.Body = http.MaxBytesReader(w, r.Body, 1_048_576) //1MB limit
+
+	dec := json.NewDecoder(r.Body)
+	dec.DisallowUnknownFields() //catch unwanted fields in the JSON request body
+
 	//decode the request body into the target destination
-	err := json.NewDecoder(r.Body).Decode(dst)
+	err := dec.Decode(dst)
 	if err != nil {
 		var syntaxError *json.SyntaxError
 		var unmarshalTypeError *json.UnmarshalTypeError
 		var invalidUnmarshalError *json.InvalidUnmarshalError
+		var maxBytesError *http.MaxBytesError
 
 		switch {
 		//use the errors.As() function to check whether the error has the type *json.SyntaxError
@@ -73,12 +80,24 @@ func (app *application) readJSON(w http.ResponseWriter, r *http.Request, dst any
 			return fmt.Errorf("body contains incorrect JSON type (at character %d)", unmarshalTypeError.Offset)
 		case errors.Is(err, io.EOF):
 			return errors.New("body must not be empty")
+		//if the JSON contains a field which cannot be mapped to the target destination
+		case strings.HasPrefix(err.Error(), "json: unknown field "):
+			fieldName := strings.TrimPrefix(err.Error(), "json: unknown field ")
+			return fmt.Errorf("body contains unknown key %s", fieldName)
+		//returns if the request body exceeded our size limit of 1MB
+		case errors.As(err, &maxBytesError):
+			return fmt.Errorf("body must not be larger than %d bytes", maxBytesError.Limit)
 		//returned if we pass something that is not a non-nil pointer as the target destination
 		case errors.As(err, &invalidUnmarshalError):
 			panic(err)
 		default:
 			return err
 		}
+	}
+
+	err = dec.Decode(&struct{}{})
+	if !errors.Is(err, io.EOF) {
+		return errors.New("body must only contain a single JSON value")
 	}
 
 	return nil
--- internal/data/runtime.go
+++ internal/data/runtime.go
@@ -1,13 +1,43 @@
 package data
 
 import (
+	"errors"
 	"fmt"
 	"strconv"
+	"strings"
 )
+
+// define an error that our UnmarshalJSON() method can return if we're unable to parse or convert the JSON string successfully
+var ErrInvalidRuntimeFormat = errors.New("invalid runtime format")
 
 type Runtime int32
 
-// This should return teh JSON-encoded value for the movie runtime
+// implement a UnmarshalJSON() method on the Runtime type so that it satisfies the json.Unmarshaler interface.
+func (r *Runtime) UnmarshalJSON(jsonValue []byte) error {
+	unquotedJSONValue, err := strconv.Unquote(string(jsonValue))
+	if err != nil {
+		return ErrInvalidRuntimeFormat
+	}
+	//split the string to isolate the part containing the number
+	parts := strings.Split(unquotedJSONValue, " ")
+	//sanity check the parts of the string to make sure it was in the expected format.
+	if len(parts) != 2 || parts[1] != "mins" {
+		return ErrInvalidRuntimeFormat
+	}
+
+	i, err := strconv.ParseInt(parts[0], 10, 32)
+	if err != nil {
+		return ErrInvalidRuntimeFormat
+	}
+
+	//convert the int32 to a Runtime type & assign this to the receiver. We use the * operator to dereference the receiver
+	// (which is a pointer to a Runtime type) in order to set the underlying value of the pointer.
+	*r = Runtime(i)
+
+	return nil
+}
+
+// This should return the JSON-encoded value for the movie runtime
 func (r Runtime) MarshalJSON() ([]byte, error) {
 	jsonValue := fmt.Sprintf("%d mins", r)
 
